5260d09556c2540ee97f93ef6543241092ba4456
Grigorii Chudnov <grigorii.chudnov@zalando.de>
2020-10-04 01:03:40 +0200

    add example

---
+++ example/src/main/scala/com/github/gchudnov/swearwolf/example/Main.scala
@@ -0,0 +1,104 @@
+package com.github.gchudnov.swearwolf.example
+
+import java.io.{ FileOutputStream, PrintStream }
+
+import com.github.gchudnov.swearwolf.Screen
+import com.github.gchudnov.swearwolf.term.{ KeySeq, SizeKeySeq }
+import com.github.gchudnov.swearwolf.util.EventLoop.Action
+import com.github.gchudnov.swearwolf.util.TextStyle._
+import com.github.gchudnov.swearwolf.util._
+import com.github.gchudnov.woods.box.{ Box, _ }
+import com.github.gchudnov.woods.graph.{ Graph, GraphStyle, _ }
+import com.github.gchudnov.woods.grid.{ Grid, GridStyle }
+import com.github.gchudnov.woods.label.Label
+import com.github.gchudnov.woods.table.{ Table, TableStyle }
+import com.github.gchudnov.woods.text._
+import com.github.gchudnov.woods.util.AlignStyle
+
+import scala.util.Using
+import scala.util.control.Exception.nonFatalCatch
+
+object Main extends App {
+
+  private val logFilePath = "~/swearwolf-example-errors.log"
+  private val posKeqSeq = Point(32, 0)
+
+  nonFatalCatch
+    .either({
+      Using.resource(Screen.acquireOrThrow()) { (sc: Screen) =>
+        val handler = eventHandler(sc)(posKeqSeq) _
+        for {
+          _ <- render(sc)
+          _ <- sc.eventLoop(EventLoop.withDefaultHandler(handler))
+        } yield ()
+      }
+    })
+    .flatten
+    .fold(writeFileLog, _ => ())
+
+  private def render(sc: Screen): Either[Throwable, Unit] = {
+    val data = List(10.0, 56.0, 25.0, 112.0, 45.9, 92.1, 8.0, 12.0, 10.0, 56.0, 25.0, 112.0, 45.9, 92.1, 8.0, 12.0)
+
+    val b  = Box(Size(21, 3), BoxStyle.SingleBorder)
+    val g1  = Graph(Size(16, 1), data, GraphStyle.Dot)
+    val g2  = Graph(Size(16, 2), data, GraphStyle.Step)
+    val g3  = Graph(Size(16, 2), data, GraphStyle.Quad)
+    val gd = Grid(Size(7, 7), Size(3, 3), GridStyle.Frame2)
+    val t  = Table(Seq(Seq("111", "222"), Seq("a", "b"), Seq("c", "d")), TableStyle.Frame)
+    val l  = Label(Size(16, 4), "this is a very long text that doesnt fit in the provided area entirely", AlignStyle.Left)
+
+    for {
+      rich <- RichText.make("<b>BOLD</b><color fg='#AA0000' bg='#00FF00'>NOR</color>MAL<i>italic</i><k>BLINK</k>")
+      _    <- sc.put(Point(0, 0), "HELLO", Bold | Foreground(NamedColor.Blue))
+      _    <- sc.put(Point(8, 0), "WORLD!", Foreground(NamedColor.Blue) | Background(NamedColor.Yellow))
+      _    <- sc.put(Point(0, 2), rich)
+      _    <- sc.put(Point(0, 4), b, Foreground(NamedColor.Blue))
+      _    <- sc.put(Point(32, 2), g1, Foreground(NamedColor.Green))
+      _    <- sc.put(Point(32, 4), g2, Foreground(NamedColor.LimeGreen))
+      _    <- sc.put(Point(32, 7), g3, Foreground(NamedColor.Azure))
+      _    <- sc.put(Point(22, 0), gd, Foreground(NamedColor.Yellow))
+      _    <- sc.put(Point(0, 7), t, Foreground(NamedColor.White))
+      _    <- sc.put(Point(0, 13), l, Foreground(NamedColor.Red))
+      _    <- sc.flush()
+    } yield ()
+  }
+
+  private def eventHandler(screen: Screen)(pos: Point)(ks: List[KeySeq]): Either[Throwable, EventLoop.Action] = {
+    // handle screen resize
+    val errOrResize = Func
+      .sequence(ks.filter(_.isInstanceOf[SizeKeySeq]).map { _ =>
+        for {
+          _ <- screen.clear()
+          _ <- render(screen)
+        } yield ()
+      })
+      .map(_ => ())
+
+    // display key codes
+    val errOrDisplay = Func
+      .sequence(ks.zipWithIndex.map({ case (keqSeq, i) =>
+        screen.put(pos.offset(0, i), keqSeq.toString)
+      }))
+      .map(_ => ())
+
+    for {
+      _ <- errOrResize
+      _ <- errOrDisplay
+      _ <- screen.flush()
+    } yield Action.Continue
+  }
+
+  private def writeStdoutLog(t: Throwable): Unit =
+    writeErrorLog(System.out)(t)
+
+  private def writeFileLog(t: Throwable): Unit = {
+    val output = new PrintStream(new FileOutputStream(logFilePath, true))
+    writeErrorLog(output)(t)
+  }
+
+  private def writeErrorLog(output: PrintStream)(t: Throwable): Unit = {
+    output.println(t.getMessage)
+    t.printStackTrace(output)
+    output.flush()
+  }
+}
