4644e56dafa5a842cf2c12c0fe02ecdd1f6c9f20
Grigorii Chudnov <grigorii.chudnov@zalando.de>
2020-10-04 19:33:16 +0200

    update named colors

--- res/colors/ListMain.scala
+++ res/colors/ListMain.scala
@@ -10,16 +10,26 @@ object ListMain extends App {
   val items = names.map(name => {
     val c = NamedColor.colors(name)
     val hex = Color.toHex(c).toUpperCase()
-    List(s" ![${hex}](https://placehold.it/15/${hex.substring(1)}/000000?text=+) ", s" ${name} ", s" ${c} ", s" ${hex} ")
+    List(s" ![${hex}](https://placehold.it/15/${hex.substring(1)}/000000?text=+) ", s" ${name} ", s" NamedColor.${toTypeName(name)} ", s" ${c} ", s" ${hex} ")
   })
 
   val header = List(
-    List(" Color ", " Name ", " Code ", " Hex "),
-    List(" --- ", " --- ", " --- ", " --- ")
+    List(" Color ", " Name ", " Named ", " Code ", " Hex "),
+    List(" --- ", " --- ", " --- ", " --- ", " --- ")
   )
 
   (header ++ items).foreach(is => {
     println(is.mkString("|", "|", "|"))
   })
+
+  private def toTypeName(name: String): String = {
+    val n1 = Range('a', 'z').foldLeft(name)((acc, ch) => {
+      acc.replace(s"-${ch.toChar}", s"${ch.toChar.toString.toUpperCase()}")
+    })
+
+    val n2 = n1.substring(0, 1).toUpperCase + n1.substring(1)
+
+    n2
+  }
 
 }
