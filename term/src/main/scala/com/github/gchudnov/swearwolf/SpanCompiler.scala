feb6d103fd6000decf67d81d993a3a244b0ff582
Grigorii Chudnov <g.chudnov@gmail.com>
2022-02-22 20:52:26 +0100

    update span compiler

--- term/src/main/scala/com/github/gchudnov/swearwolf/SpanCompiler.scala
+++ term/src/main/scala/com/github/gchudnov/swearwolf/SpanCompiler.scala
@@ -7,6 +7,8 @@ import com.github.gchudnov.swearwolf.util.spans.TextSpan
 import com.github.gchudnov.swearwolf.util.spans.ByteSpan
 import com.github.gchudnov.swearwolf.util.data.Stack
 import com.github.gchudnov.swearwolf.util.colors.Color
+import com.github.gchudnov.swearwolf.util.styles.TextStyle
+import com.github.gchudnov.swearwolf.util.styles.TextStyleSeq
 
 /**
  * Transforms Span to an array of Bytes that can be sent to the terminal.
@@ -23,9 +25,22 @@ object SpanCompiler:
     blink: Int = 0,
     invert: Int = 0,
     strikethrough: Int = 0,
-    transient: Int = 0,
+    transparent: Int = 0,
     noColor: Int = 0
-  )
+  ):
+    def withStyle(style: TextStyle): State = style match
+      case TextStyle.Empty             => copy(empty = empty + 1)
+      case TextStyle.Bold              => copy(bold = bold + 1)
+      case TextStyle.Italic            => copy(italic = italic + 1)
+      case TextStyle.Underline         => copy(underline = underline + 1)
+      case TextStyle.Blink             => copy(blink = blink + 1)
+      case TextStyle.Invert            => copy(invert = invert + 1)
+      case TextStyle.Strikethrough     => copy(strikethrough = strikethrough + 1)
+      case TextStyle.Transparent       => copy(transparent = transparent + 1)
+      case TextStyle.NoColor           => copy(noColor = noColor + 1)
+      case TextStyle.Foreground(color) => copy(fgColor = fgColor.push(color))
+      case TextStyle.Background(color) => copy(bgColor = bgColor.push(color))
+      case TextStyleSeq(styles)        => styles.foldLeft(this)(_.withStyle(_))
 
   private object State:
     val empty: State =
