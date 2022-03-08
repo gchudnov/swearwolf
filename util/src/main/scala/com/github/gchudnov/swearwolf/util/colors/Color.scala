f5448720a115c89cd37538269940eb38d638ae79
Grigorii Chudnov <g.chudnov@gmail.com>
2022-03-08 14:49:44 +0100

    make color generic

--- util/src/main/scala/com/github/gchudnov/swearwolf/util/colors/Color.scala
+++ util/src/main/scala/com/github/gchudnov/swearwolf/util/colors/Color.scala
@@ -1,6 +1,7 @@
 package com.github.gchudnov.swearwolf.util.colors
 
 import com.github.gchudnov.swearwolf.util.show.Show
+import com.github.gchudnov.swearwolf.util.func.MonadError
 
 final case class Color(r: Int, g: Int, b: Int):
   def toHex(): String =
@@ -22,27 +23,28 @@ object Color:
    *    RRGGBB
    * }}}
    */
-  def parse(value: String): Either[Throwable, Color] =
-    if value.isEmpty then Left(new ColorException("Cannot parse the color: <empty>"))
-    else fromName(value).orElse(fromHex(value))
+  def parse[F[_]: MonadError](value: String): F[Color] =
+    import MonadError.*
+    if value.isEmpty then summon[MonadError[F]].error(new ColorException("Cannot parse the color: <empty>"))
+    else fromName[F](value).handleErrorWith(_ => fromHex(value))
 
   given Show[Color] with
     extension (a: Color)
       def show: String =
         a.toHex()
 
-  private def fromName(name: String): Either[Throwable, Color] =
+  private def fromName[F[_]: MonadError](name: String): F[Color] =
     require(name.nonEmpty, "Color Name must be non-empty")
     val normalizedName = name.toLowerCase.replaceAll("_", "-")
-    colors.get(normalizedName).fold(Left(new ColorException(s"Named Color is not found: $name")): Either[Throwable, Color])(c => Right(c))
+    colors.get(normalizedName).fold(summon[MonadError[F]].error(new ColorException(s"Named Color is not found: $name")): F[Color])(c => summon[MonadError[F]].pure(c))
 
-  private def fromHex(value: String): Either[Throwable, Color] =
+  private def fromHex[F[_]: MonadError](value: String): F[Color] =
     require(value.nonEmpty, "Color Value must be non-empty")
     val c = if value.head == '#' then value.tail else value
-    if c.length != 6 then Left(new ColorException("Cannot parse the color: invalid format. Supported formats: (#RRGGBB, RRGGBB)"))
+    if c.length != 6 then summon[MonadError[F]].error(new ColorException("Cannot parse the color: invalid format. Supported formats: (#RRGGBB, RRGGBB)"))
     else
       val cs = c.grouped(2).map(it => Integer.parseInt(it, 16)).toSeq
-      Right(Color(cs(0), cs(1), cs(2)))
+      summon[MonadError[F]].pure(Color(cs(0), cs(1), cs(2)))
 
   val AliceBlue: Color            = Color(240, 248, 255)
   val AntiqueWhite: Color         = Color(250, 235, 215)
