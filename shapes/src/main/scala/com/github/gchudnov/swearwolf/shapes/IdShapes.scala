4df7d8a7eeb4a12b2eaef06aefe8a67d3cbc3bba
Grigorii Chudnov <g.chudnov@gmail.com>
2022-03-15 23:56:48 +0100

    refactor

---
+++ shapes/src/main/scala/com/github/gchudnov/swearwolf/shapes/IdShapes.scala
@@ -0,0 +1,6 @@
+package com.github.gchudnov.swearwolf.shapes
+
+import com.github.gchudnov.swearwolf.util.func.IdMonad
+import com.github.gchudnov.swearwolf.util.func.Identity
+
+object IdShapes extends AllShapes[Identity]
---
+++ shapes/src/main/scala/com/github/gchudnov/swearwolf/shapes/TryShapes.scala
@@ -0,0 +1,7 @@
+package com.github.gchudnov.swearwolf.shapes
+
+import com.github.gchudnov.swearwolf.util.func.TryMonad
+
+import scala.util.Try
+
+object TryShapes extends AllShapes[Try]
