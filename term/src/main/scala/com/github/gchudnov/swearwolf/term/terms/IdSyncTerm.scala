83c19268120797406e4b68cc86952fa1b43e3a77
Grigorii Chudnov <g.chudnov@gmail.com>
2022-03-22 19:58:52 +0100

    upd term

--- term/src/main/scala/com/github/gchudnov/swearwolf/term/terms/IdSyncTerm.scala
+++ term/src/main/scala/com/github/gchudnov/swearwolf/term/terms/IdSyncTerm.scala
@@ -1,9 +1,11 @@
 package com.github.gchudnov.swearwolf.term.terms
 
-import com.github.gchudnov.swearwolf.util.func.IdMonad
-import com.github.gchudnov.swearwolf.util.func.Identity
+import com.github.gchudnov.swearwolf.term.Term
+import com.github.gchudnov.swearwolf.util.func.{ IdMonad, Identity }
 
 import java.io.{ InputStream, OutputStream }
+import java.nio.file.Path
+import java.time.format.DateTimeFormatter
 
 final class IdSyncTerm(in: InputStream, out: OutputStream, isClose: Boolean) extends SyncTerm[Identity](in = in, out = out, isClose = isClose)
 
@@ -11,3 +13,11 @@ object IdSyncTerm:
 
   def make(in: InputStream = System.in, out: OutputStream = System.out): IdSyncTerm =
     new IdSyncTerm(in = in, out = out, isClose = false)
+
+  def fileLog(
+    path: Path,
+    term: Term[Identity] = make(),
+    isTruncate: Boolean = true,
+    fmt: DateTimeFormatter = LogTerm.defaultDateTimeFormatter
+  ): Term[Identity] =
+    LogTerm.fileLog(term, path, isTruncate, fmt)
--- ziox/term/src/main/scala/com/github/gchudnov/swearwolf/zio/term/ZioTerm.scala
+++ ziox/term/src/main/scala/com/github/gchudnov/swearwolf/zio/term/ZioTerm.scala
@@ -1,19 +1,33 @@
 package com.github.gchudnov.swearwolf.zio.term
 
 import com.github.gchudnov.swearwolf.term.Term
+import com.github.gchudnov.swearwolf.term.terms.LogTerm
 import com.github.gchudnov.swearwolf.zio.term.internal.AsyncZioTerm
+import com.github.gchudnov.swearwolf.zio.util.func.RIOMonadAsyncError
 import zio.*
 
 import java.io.{ InputStream, OutputStream }
 import java.lang.System
+import java.nio.file.Path
+import java.time.format.DateTimeFormatter
 
 type ZioTerm = AsyncZioTerm
 
 object ZioTerm:
 
   def layer(in: InputStream = System.in, out: OutputStream = System.out): ULayer[ZioTerm] =
-    val term = new AsyncZioTerm(in, out, false)
-    ZLayer.succeed(term)
+    ZLayer.succeed(make(in, out))
 
   def stdIoLayer: ULayer[ZioTerm] =
     layer()
+
+  def fileLogLayer(
+    path: Path,
+    term: Term[Task] = make(System.in, System.out),
+    isTruncate: Boolean = true,
+    fmt: DateTimeFormatter = LogTerm.defaultDateTimeFormatter
+  ): Term[Task] =
+    LogTerm.fileLog(term, path, isTruncate, fmt)
+
+  private def make(in: InputStream, out: OutputStream): AsyncZioTerm =
+    new AsyncZioTerm(in, out, false)
