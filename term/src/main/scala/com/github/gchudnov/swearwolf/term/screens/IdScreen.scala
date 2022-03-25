8e0ef87a33000d8a2ab187460fd1923aeff7cdb0
Grigorii Chudnov <g.chudnov@gmail.com>
2022-03-25 15:13:56 +0100

    refactor screens

--- term/src/main/scala/com/github/gchudnov/swearwolf/term/screens/IdScreen.scala
+++ term/src/main/scala/com/github/gchudnov/swearwolf/term/screens/IdScreen.scala
@@ -1,10 +1,8 @@
 package com.github.gchudnov.swearwolf.term.screens
 
-import com.github.gchudnov.swearwolf.term.{Screen, Term}
 import com.github.gchudnov.swearwolf.term.Term.TermAction
-import com.github.gchudnov.swearwolf.util.func.{IdMonad, Identity}
-import com.github.gchudnov.swearwolf.util.func.MonadError
-import com.github.gchudnov.swearwolf.util.func.IdMonad
+import com.github.gchudnov.swearwolf.term.{ Screen, Term }
+import com.github.gchudnov.swearwolf.util.func.{ IdMonad, Identity, MonadError }
 
 final class IdScreen(term: Term[Identity], cleanup: TermAction[Identity]) extends SyncShellScreen(term, cleanup) {}
 
@@ -13,23 +11,6 @@ object IdScreen:
     import MonadError.*
 
     val cleanupM: Identity[TermAction[Identity[*]]] = SyncShellScreen.initSync(term)
-    val screenM = cleanupM.map(cleanup => new IdScreen(term, cleanup))
+    val screenM                                     = cleanupM.map(cleanup => new IdScreen(term, cleanup))
 
     screenM
-
-//    val c: Identity[TermAction[Identity[*]]] = SyncShellScreen.initSync(term)
-//
-//    val cp: TermAction[Identity] = ???
-////
-////    val s   = new IdScreen(term, cp)
-//
-//    val x = c.map(it => {
-//      ???
-//    })
-
-    ???
-
-//    for
-//      cleanup <- SyncShellScreen.initSync(term)
-//      screen   = new IdScreen(term, cleanup)
-//    yield screen
