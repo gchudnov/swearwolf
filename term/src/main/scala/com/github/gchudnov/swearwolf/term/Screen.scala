80ddd0d7cb363ad85d54eb943a7232b27775dd0a
Grigorii Chudnov <g.chudnov@gmail.com>
2022-03-26 09:43:49 +0100

    refactor screens

--- term/src/main/scala/com/github/gchudnov/swearwolf/term/Screen.scala
+++ term/src/main/scala/com/github/gchudnov/swearwolf/term/Screen.scala
@@ -1,14 +1,16 @@
 package com.github.gchudnov.swearwolf.term
 
 import com.github.gchudnov.swearwolf.term.EventLoop.KeySeqHandler
-import com.github.gchudnov.swearwolf.term.{ EscSeq, EventLoop, Term }
 import com.github.gchudnov.swearwolf.term.keys.KeySeq
-import com.github.gchudnov.swearwolf.term.screens.{ EitherScreen, TryScreen }
+import com.github.gchudnov.swearwolf.term.screens.{ EitherScreen, FutureScreen, IdScreen, TryScreen }
+import com.github.gchudnov.swearwolf.term.{ EscSeq, EventLoop, Term }
 import com.github.gchudnov.swearwolf.util.bytes.Bytes
+import com.github.gchudnov.swearwolf.util.func.Identity
 import com.github.gchudnov.swearwolf.util.geometry.{ Point, Size }
 import com.github.gchudnov.swearwolf.util.spans.Span
 import com.github.gchudnov.swearwolf.util.styles.TextStyle
 
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.Try
 
 trait Screen[F[_]] extends Writer[F]:
@@ -24,22 +26,11 @@ object Screen:
   def syncEither(term: Term[Either[Throwable, *]]): Either[Throwable, Screen[Either[Throwable, *]]] =
     EitherScreen.make(term)
 
-  def syncId() =
-    ???
+  def syncId(term: Term[Identity]): Identity[Screen[Identity]] =
+    IdScreen.make(term)
 
   def syncTry(term: Term[Try]): Try[Screen[Try]] =
     TryScreen.make(term)
 
-  def asyncFuture() =
-    ???
-
-  // TODO: add more methods, similar to Term
-
-//   def make(term: Term): Either[Throwable, Screen] =
-//     TermScreen.make(term)
-
-//   def array(size: Size, cellChar: Char = ArrayScreen.DefaultCellChar, borderChar: Option[Char] = Some(ArrayScreen.DefaultBorderChar)): Either[Throwable, Screen] =
-//     Right(ArrayScreen.make(size, cellChar, borderChar))
-
-//   def compile(span: Span): Bytes =
-//     TermScreen.compile(span)
+  def asyncFuture(term: Term[Future])(using ec: ExecutionContext): Future[Screen[Future]] =
+    FutureScreen.make(term)
---
+++ term/src/main/scala/com/github/gchudnov/swearwolf/term/screens/FutureScreen.scala
@@ -0,0 +1,20 @@
+package com.github.gchudnov.swearwolf.term.screens
+
+import com.github.gchudnov.swearwolf.term.Term.TermAction
+import com.github.gchudnov.swearwolf.term.{ Screen, Term }
+import com.github.gchudnov.swearwolf.util.func.{ FutureMonad, MonadError }
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+/**
+ * NOTE: it is safe to inherit from SyncScreen since Future is eager.
+ */
+final class FutureScreen(term: Term[Future], cleanup: TermAction[Future])(using ec: ExecutionContext) extends SyncShellScreen(term, cleanup) {}
+
+object FutureScreen:
+  def make(term: Term[Future])(using ec: ExecutionContext): Future[FutureScreen] =
+    import MonadError.*
+    for
+      cleanup <- SyncShellScreen.initSync(term)
+      screen   = new FutureScreen(term, cleanup)
+    yield screen
